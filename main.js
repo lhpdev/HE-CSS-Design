33d993887af4f62c17ba7174a55a4e0d2829fe9d
Lucas <lucass.hauptmann@gmail.com>
2018-09-21 19:43:05 -0300

    Add Sass to project: *Create main.scss file *Improve grid rules *Add sass variables to colors (ongoing)

--- main.js
+++ main.js
@@ -35,6 +35,22 @@ logViewerApplication.style.visibility = 'hidden';
 
 var current_date = Date();
 
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    m = checkTime(m);
+    document.getElementById('left-panel-clock').innerHTML =
+    h + ":" + m;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+
+startTime();
+
 gatewayIcon.addEventListener("click", function() {
     gateway_clicked = !gateway_clicked;
     if (gateway_clicked) {
@@ -244,10 +260,7 @@ notificationsGlobalIcon.addEventListener("click", function() {
     }
 });
 
-{
-    // Make the DIV element draggable:
-    dragElement(document.getElementById("logviewer"));
-    function dragElement(elmnt) {
+function dragElement(elmnt) {
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
     if (document.getElementById(elmnt.id + "header")) {
         // if present, the header is where you move the DIV from:
@@ -286,51 +299,10 @@ notificationsGlobalIcon.addEventListener("click", function() {
         document.onmouseup = null;
         document.onmousemove = null;
     }
-    }
-};
+}
 
-{
-    // Make the DIV element draggable:
-    dragElement(document.getElementById("notifications"));
-    function dragElement(elmnt) {
-    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    if (document.getElementById(elmnt.id + "header")) {
-        // if present, the header is where you move the DIV from:
-        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-    } else {
-        // otherwise, move the DIV from anywhere inside the DIV:
-        elmnt.onmousedown = dragMouseDown;
-    }
+const notification = "notifications";
+dragElement(document.getElementById(notification));
 
-    function dragMouseDown(e) {
-        e = e || window.event;
-        e.preventDefault();
-        // get the mouse cursor position at startup:
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        document.onmouseup = closeDragElement;
-        // call a function whenever the cursor moves:
-        document.onmousemove = elementDrag;
-    }
-
-    function elementDrag(e) {
-        e = e || window.event;
-        e.preventDefault();
-        // calculate the new cursor position:
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        // set the element's new position:
-        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-    }
-
-    function closeDragElement() {
-        // stop moving when mouse button is released:
-        document.onmouseup = null;
-        document.onmousemove = null;
-    }
-    }
-};
-
+const logviewer = "logviewer";
+dragElement(document.getElementById(logviewer));
